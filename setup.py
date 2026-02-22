825bcd650e9069bc513ec23cc7298fc10035dd80
Joshua Davis <joshuadavis@microsoft.com>
2026-02-21 20:51:29 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
