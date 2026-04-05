6037d4c7f092f0283c4941c0a70cdd86b41fd90f
Joshua Davis <me@jdav.is>
2026-04-05 18:52:34 -0400

    Sidebar: move Governance to end, restore H3 header

--- scripts/generate_wiki_governance.py
+++ scripts/generate_wiki_governance.py
@@ -413,7 +413,7 @@ def _generate_sidebar(
     lines: list[str] = []
     lines.append(marker_start)
     lines.append("")
-    lines.append("**Governance**")
+    lines.append("### Governance")
     lines.append("")
 
     # Split policies into Azure vs non-Azure groups
